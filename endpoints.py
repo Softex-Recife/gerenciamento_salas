c9d08355781c3fdc313cbdad575f5d4775f53d54
Delando Junior <delandosmjunior@gmail.com>
2020-01-29 16:18:05 -0300

    only post methods

--- endpoints.py
+++ endpoints.py
@@ -11,24 +11,23 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST', 'OPTIONS'])
 def login():
-    
     response_dict = {"nome_empresa" : "Softex-Recife", "liberar": "0", "hora_inicio" : "15:00", "hora_fim" : "16:00", "mensagem": "Não existe reserva"}
-
-    room = request.json.cod_sala_reuniao
-    password = request.json.senha
-    event = dao.read(room)
+    if(request.method == "POST"):
+        room = request.json["cod_sala_reuniao"]
+        password = request.json["senha"]
+        print(room, password)
+        event = dao.read(room)
+        if event:
+            event_pwd = event["password"]
+            if event_pwd == password:
+                response_dict["nome_empresa"] = event["name"]
+                response_dict["liberar"] = "1"
+                response_dict["hora_inicio"] = event["start"]
+                response_dict["hora_fim"] = event["end"]
+            else:
+                response_dict["mensagem"] = "Senha incorreta"
     
-    if event:
-        event_pwd = event.password
-        if event_pwd == password:
-            response_dict["nome_empresa"] = event.name
-            response_dict["liberar"] = "1"
-            response_dict["hora_inicio"] = event.start
-            response_dict["hora_fim"] = event.end
-        else:
-            response_dict["mensagem"] = "Senha incorreta"
-        
-    res = Response(json.dumps(response_dict))
+    res = Response(json.dumps(response_dict)) 
     res.headers["Access-Control-Allow-Origin"] = '*'
     res.headers["Access-Control-Allow-Headers"] = '*'
     return res
